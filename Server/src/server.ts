082346478356596fe7728654f33af565632491c9
RohitChoukiker <rohitchoukiker2803@gmail.com>
2025-12-27 23:13:38 +0530

    add initial setup

---
+++ Server/src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/', async (request: any, reply: any) => {
+  return { hello: 'world' };
+});
+
+const start = async () => {
+  try {
+    await app.listen({ port: 3000, host: '0.0.0.0' });
+    console.log('Server listening on http://localhost:3000');
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
