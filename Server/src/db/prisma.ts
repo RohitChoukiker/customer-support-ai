7091d805517d987955d0fd32ff721e9276e2f183
RohitChoukiker <rohitchoukiker2803@gmail.com>
2025-12-29 11:35:26 +0530

    up

--- Server/src/db/prisma.ts
+++ Server/src/db/prisma.ts
@@ -1,3 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+let prisma: PrismaClient;
+
+export function getPrisma() {
+  if (!prisma) prisma = new PrismaClient();
+  return prisma;
+}
